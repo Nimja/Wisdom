8eb0489ec09766f43ebc1776e37537ba874423d4
Remy <remy@emesa.nl>
2018-07-09 13:44:58 +0200

    Anniversary improvements to allow for different messages and options.

--- wisdom/anniversary.js
+++ wisdom/anniversary.js
@@ -1,5 +1,6 @@
 // Load the speak packages.
 var fs = require("fs");
+var messages = require('./data/anniversary_texts.json');
 
 var Anniversary = function (client) {
     this.client = client;
@@ -20,15 +21,16 @@ Anniversary.prototype = {
             var anniversary = anniversaries[index];
             // We fetch users via the channel.guild so that we don't announce people who are no longer in the guild.
             guild.fetchMember(anniversary.id, false)
-                .then((member) => {
-                    var user = member.user;
-                    var year = anniversary.age > 1 ? ' years' : ' year';
-                    channel.send(user.toString() + " has been supporting me for " + anniversary.age + year + ', today! Thank you so much!');
-                })
-                .catch((error) => {
+                    .then((member) => {
+                        var user = member.user;
+                        var year = anniversary.age > 1 ? ' years' : ' year';
+                        var message = getMessage(user.toString(), anniversary.age, year);
+                        channel.send(message);
+                    })
+                    .catch((error) => {
                         console.log(error);
                     }
-                ); // Do nothing in case it's not found/invalid.
+                    ); // Do nothing in case it's not found/invalid.
             break;
         }
     },
@@ -36,7 +38,7 @@ Anniversary.prototype = {
      * Get anniversaries with userId and age.
      * @returns {Array}
      */
-    getAnniversaries: function() {
+    getAnniversaries: function () {
         var data = JSON.parse(fs.readFileSync('wisdom/data/anniversaries.json'));
         var curDate = new Date();
         var result = [];
@@ -50,7 +52,25 @@ Anniversary.prototype = {
         }
         return result;
     },
-    init: function(channel) {
+    init: function (channel) {
         this.channel = channel;
     }
 };
+
+/**
+ * Get (random) message for anniversary.
+ * @param {String} user
+ * @param {Number} number
+ * @param {String} year
+ * @returns {String}
+ */
+function getMessage(user, number, year)
+{
+    var message = messages[Math.floor(Math.random() * messages.length)];
+    var map = {USER: user, NUMBER: number, YEAR: year};
+    var re = new RegExp(Object.keys(map).join("|"), "g");
+    var parsed = message.replace(re, function (matched) {
+        return map[matched];
+    });
+    return parsed;
+}
