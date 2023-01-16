f79069b1a8ca6ebb3ac81e5abe0b98edb3f34cdc
Remy <remy@emesa.nl>
2023-01-16 10:14:13 +0100

    Updated packages, added some lines.

--- wisdom/commands/file.js
+++ wisdom/commands/file.js
@@ -21,21 +21,27 @@ const schedule = require('node-schedule');
 schedule.scheduleJob('2 20 * * *', updateCache);
 
 
-var cache = {};
-var hour = 3600000;
+var cache = {}; // Local cache, to avoid having to do network requests every search.
 
 updateCache();
 
 function handle(interaction) {
-    var rest = interaction.options.get('file');
-    rest = rest ? rest.value.toLowerCase().trim() : '';
+    var searchText = interaction.options.get('file');
+    searchText = searchText ? searchText.value.toLowerCase().trim() : '';
 
+    searchText = cleanRegex(searchText);
     var filter = cache.content.filter;
     var file = false;
-    if (rest === 'last') {
+    if (searchText === 'last') {
         file = cache.content.files[0]
-    } else if (rest.length > 0) {
-        file = searchFile(rest)
+    } else if (searchText.length > 0) {
+        if (searchText.match(/^[0-9]+$/)) { // All numbers, assume file.
+            searchText = 'f' + searchText
+        }
+        file = searchFileId(searchText);
+        if (!file) {
+            file = searchFile(searchText);
+        }
     } else {
         file = cache.content.files[Math.floor(Math.random() * cache.content.files.length)];
     }
@@ -43,41 +49,93 @@ function handle(interaction) {
         message = formatFile(filter, file);
         return message
     } else {
-        return {
-            "content": "I couldn't find anything for: " + rest,
-            "ephemeral": true,
-        };
+        return formatNotFound(searchText);
     }
 }
 
-function searchFile(rest) {
-    var result = [];
+/**
+ * Clean up the search from regex characters that can break.
+ */
+function cleanRegex(searchText) {
+    return searchText.toString().replace(/[\(\)\[\]\?]/gm, ' ');
+}
+
+/**
+ * Search for file IDs.
+ */
+function searchFileId(searchText) {
+    if (searchText in cache.indexes) {
+        return cache.content.files[cache.indexes[searchText]];
+    }
+    return false;
+}
+
+/**
+ * Search for text in files.
+ */
+function searchFile(searchText) {
     for (var i in cache.content.files) {
         var file = cache.content.files[i];
-        if (file.search.search(rest) > -1) {
-            return file;
+        try {
+            if (file.search.search(searchText) > -1) {
+                return file;
+            }
+        } catch (error) {
+            console.log("Search error:", error.message);
+            break;
         }
     }
     return false;
 }
 
-function formatFile(filter, file) {
+function formatNotFound(searchText) {
     let message = {
-        content: file.name + ' - ' + file.links.details,
+        content: "I couldn't find anything for: " + searchText,
         ephemeral: true,
     };
     let embed = {
         "color": 4215449,
         "fields": [
-            { name: 'Description', value: file.description },
-            { name: 'Intended effect', value: file.intended },
-            { name: 'Length', value: secondsToTime(file.length) }
+            { name: 'Help', value: "You can search using parts of words and very basic regex (no brackets)" },
+            {
+                name: 'File number', value: [
+                    "A file number on its own is assumed to be one of the normal files.",
+                    "`10` for '10 - Fun...'",
+                ].join("\n")
+            },
+            {
+                name: 'Short codes', value: [
+                    "Use short-codes (same as playlist url) to find specific files:",
+                    "`f10` for '10 - Fun...'",
+                    "`l10` for 'Live 10 - Brainwashing...'",
+                    "`i10` for 'Live Light 10 - Incubus...'",
+                ].join("\n")
+            },
         ]
     }
+    message.embeds = [embed];
+    return message;
 
+}
+
+/**
+ * Search for file IDs.
+ */
+function formatFile(filter, file) {
+    let message = {
+        content: file.name + ' - ' + file.links.details,
+        ephemeral: true,
+    };
+    var embed = {
+        "color": 4215449,
+        "fields": [
+            { name: 'Description', value: file.description },
+            { name: 'Intended effect', value: file.intended },
+            { name: 'Length', value: secondsToTime(file.length), inline: true }
+        ]
+    }
     applyFilterTags(filter.category, ['Category', 'Categories'], file.tags, embed);
     applyFilterTags(filter.feature, ['Feature', 'Features'], file.tags, embed);
-    applyOtherTags(filter.tags, file.tags, embed);
     message.embeds = [embed];
     return message;
 }
@@ -88,9 +146,13 @@ function secondsToTime(seconds) {
     return result.replace(/^00\:/, '');
 }
 
+/**
+ * Add the tags with support for plural names.
+ */
 function applyFilterTags(filters, names, filetags, embed) {
     var result = [];
     for (var tag in filters) {
+        tag = parseInt(tag);
         if (filetags.indexOf(tag) > -1) {
             result.push(filters[tag]);
         }
@@ -99,12 +161,26 @@ function applyFilterTags(filters, names, filetags, embed) {
         return;
     }
     var name = result.length == 1 ? names[0] : names[1];
-    embed.fields.push({ name: name, value: result.join(', ') });
+    // Add to the embed list.
+    embed.fields.push(
+        {
+            name: name,
+            value: result.join(', '),
+            inline: true
+        }
+    );
 }
 
+/**
+ * Apply the tags.
+ */
 function applyOtherTags(tags, filetags, embed) {
+    let skipTags = [
+        'length', 'platform', 'category', 'feature'
+    ];
+    console.log(skipTags, tags);
     for (var name in tags) {
-        if (name == 'Length' || name == 'Platform') {
+        if (skipTags.indexOf(name) > -1) {
             continue;
         }
         var cur = null;
@@ -112,6 +188,7 @@ function applyOtherTags(tags, filetags, embed) {
             if (tag[1] == '0' || tag[1] == '1') {
                 continue;
             }
+            tag = parseInt(tag);
             if (filetags.indexOf(tag) > -1) {
                 cur = tags[name][tag];
             }
@@ -122,6 +199,9 @@ function applyOtherTags(tags, filetags, embed) {
     }
 }
 
+/**
+ * Update the cache, this calls the Nimja Hypnosis site to get all the file data.
+ */
 function updateCache() {
     request('https://hypno.nimja.com/app', function (error, response, body) {
         if (error) {
@@ -130,16 +210,21 @@ function updateCache() {
         var data = JSON.parse(body);
         if (data) {
             cache = data;
-            createTextSearch();
-            cache.time = new Date(cache.time * 1000);
+            parseCacheForSearchAndLookup();
         }
     });
 }
 
-function createTextSearch() {
+/**
+ * Compile the search text, and create a lookup for ID to index.
+ */
+function parseCacheForSearchAndLookup() {
+    let indexes = {};
     for (var i in cache.content.files) {
         var file = cache.content.files[i];
         var search = file.name; // + ' ' + file.description + ' ' + file.intended;
         file.search = search.toLowerCase();
+        indexes[file.id] = i;
     }
+    cache.indexes = indexes;
 }
